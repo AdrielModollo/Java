2c813a1973f5bc318b5c4d43e2fa581ab54daa9b
Adriel <adrielmodollo@outlook.com>
2021-09-13 20:05:43 -0300

    Recuperando a porta da Instancia Execução

--- 05-cambio-service/src/main/java/br/com/adriel/controller/CambioController.java
+++ 05-cambio-service/src/main/java/br/com/adriel/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.adriel.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ import br.com.adriel.model.Cambio;
 @RequestMapping("cambio-service")
 public class CambioController {
 	
+	@Autowired
+	private Environment environment;
+	
 	//http://localhost:8000/cambio-service/5/USD/BRL
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio (
@@ -20,7 +25,10 @@ public class CambioController {
 			@PathVariable("from") String from,
 			@PathVariable("to") String to
 			) {
+		
+		var port = environment.getProperty("local.server.port");
+		
 		return new Cambio(
-				1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+				1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
