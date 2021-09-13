4745f15b38d31d18b1e4d32ff1e59d0a76b4a759
Adriel <adrielmodollo@outlook.com>
2021-09-13 20:42:18 -0300

    Suficiente para gerar nosso sql

--- 05-cambio-service/src/main/java/br/com/adriel/repository/CambioRepository.java
+++ 05-cambio-service/src/main/java/br/com/adriel/repository/CambioRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.adriel.model.Cambio;
 
 public interface CambioRepository extends JpaRepository <Cambio, Long>{
-
+	
+	Cambio findByFromAndTo(String from, String to);
 }
