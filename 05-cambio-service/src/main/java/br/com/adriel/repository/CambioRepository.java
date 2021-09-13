1441c8db772fd04f7499e62133c1c69e175ac265
Adriel <adrielmodollo@outlook.com>
2021-09-13 20:41:01 -0300

    Até aqui já seria suficiente para um CRUD

---
+++ 05-cambio-service/src/main/java/br/com/adriel/repository/CambioRepository.java
@@ -0,0 +1,9 @@
+package br.com.adriel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.adriel.model.Cambio;
+
+public interface CambioRepository extends JpaRepository <Cambio, Long>{
+
+}
